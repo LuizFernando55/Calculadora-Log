3b56ce1a6fe05620ee2d5827dbb8f64697483cd7
LuizFernando55 <45644952+LuizFernando55@users.noreply.github.com>
2019-02-22 18:50:15 -0300

    logarithms_v2.py

--- logarithms.py
+++ logarithms.py
@@ -1,21 +1,37 @@
-#coding: utf-8
+# coding: utf-8
 __author__ = 'Luiz Fernando SS'
+# Logarithms_v2
+
 print("\033[34mMonte seu Logarítimo\033[m")
+
 print("""Selecione o produto desejado: 
 [ 1 ] Base
 [ 2 ] Expoente
 [ 3 ] Argumento""")
+
 option = int(input("A opção desejada: "))
+
 if option == 1:
-    exp = float(input("Digite o expoente: "))
-    arg = float(input("Digite o argumento: "))
+    exp = int(input("Digite o expoente: "))
+    arg = int(input("Digite o argumento: "))
+
     produto = arg**1/exp
+
 if option == 2:
-    base = float(input("Digite a base: "))
-    arg = float(input("Digite o argumento: "))
-    #Todo Descobrir o valor do expoente: de base e arg já definidos
+    base = int(input("Digite a base: "))
+    arg = int(input("Digite o argumento: "))
+
+    exp = 0
+    for e in range(0, arg, base):
+        exp += 1
+
+    produto = exp
+
 if option == 3:
-    base = float(input("Digite a base: "))
-    exp = float(input("Digite o expoente: "))
+    base = int(input("Digite a base: "))
+    exp = int(input("Digite o expoente: "))
+
     produto = base**exp
+
 print("O Produto desse log foi:", produto)
+
